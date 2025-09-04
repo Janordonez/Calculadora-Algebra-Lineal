eadb46d937fe1331bef160b8a3299f44226f3584
Janordonez <168487008+Janordonez@users.noreply.github.com>
2025-09-03 19:23:54 -0600

    Validaciones

--- warnings_1.py
+++ warnings_1.py
@@ -3,5 +3,5 @@ from PyQt5.QtWidgets import QMessageBox
 def mostrarAviso(valor):
     msg = QMessageBox()
     msg.setWindowTitle("Error!")
-    msg.setText("Numero Invalido!" + " " + str(valor))
+    msg.setText("Campo vacio o Caracter inválido!" + " " + str(valor))
     msg.exec_()
